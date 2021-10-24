4a097b78d13a88d0f942669d4055234f9546fe1d
Scott Houseman <scott.houseman@gmail.com>
2021-10-24 23:33:27 +0200

    Test producer

--- producer/send.py
+++ producer/send.py
@@ -4,6 +4,7 @@ import os
 from dataclasses import dataclass
 from typing import Optional
 
+import click
 import dotenv
 import html2text
 from google.api_core.exceptions import NotFound as TopicNotFoundException
@@ -11,14 +12,11 @@ from google.cloud import pubsub_v1
 
 logger = logging.Logger(__name__)
 
-dotenv.load_dotenv(".env")
-GCLOUD_PROJECT_ID = os.environ.get("GCLOUD_PROJECT_ID")
-PUBSUB_TOPIC_ID = os.environ.get("PUBSUB_TOPIC_ID")
-
 
 @dataclass
 class MailMessage:
     recipient: str
+    sender: str
     subject: str
     html_content: str
     text_content: Optional[str] = None
@@ -27,9 +25,27 @@ class MailMessage:
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
+def get_env_file_path() -> str:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(dir_path, ".env")
+
+
+@click.command()
+@click.option(
+    "--to", "-t", "recipient", required=True, type=str, help="Recipient email address"
+)
+@click.option("--subject", "-s", required=True, type=str, help="Email subject")
+@click.option(
+    "--message",
+    "-m",
+    "html_content",
+    required=True,
+    type=str,
+    help="Email message (HTML)",
+)
 def send_email_task(
     recipient: str, subject: str, html_content: str, text_content: Optional[str] = None
-):
+) -> Optional[str]:
     """sends an email"""
 
     if not text_content:
@@ -39,6 +55,7 @@ def send_email_task(
 
     message = MailMessage(
         recipient=recipient,
+        sender=FROM_ADDR,
         subject=subject,
         html_content=html_content,
         text_content=text_content,
@@ -46,27 +63,27 @@ def send_email_task(
     return send_message(message)
 
 
-def send_message(message: MailMessage):
+def send_message(message: MailMessage) -> Optional[str]:
     message_json = message.to_json()
 
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(GCLOUD_PROJECT_ID, PUBSUB_TOPIC_ID)
-    print(f"topic_path: {topic_path}")
     try:
         future = publisher.publish(topic_path, message_json.encode("utf-8"))
         result = future.result()
-        print(f"{result}")
+        print(f"Publisheed message ID {result} to topic {topic_path}")
 
         return result
     except TopicNotFoundException as pubsub_error:
-        logger.warn(f"Error sending pubsub: {pubsub_error}")
+        logger.error(f"Error sending pubsub: {pubsub_error}")
 
         return None
 
 
 if __name__ == "__main__":
-    send_email_task(
-        recipient="scott.houseman@gmail.com",
-        subject="Test from Python",
-        html_content="<h1>Hello</h1>",
-    )
+    dotenv.load_dotenv(get_env_file_path())
+    GCLOUD_PROJECT_ID = os.environ.get("GCLOUD_PROJECT_ID")
+    PUBSUB_TOPIC_ID = os.environ.get("PUBSUB_TOPIC_ID")
+    FROM_ADDR = str(os.environ.get("FROM_ADDR"))
+
+    send_email_task()
